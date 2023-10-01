13b9ebdb9b34358e9d8d700757bca2541f04b7ce
Saketh Kotagiri <saki@Sakeths-MacBook-Pro.local>
2023-10-01 03:52:09 -0400

    css crap

--- src/app/components/Tile.js
+++ src/app/components/Tile.js
@@ -1,6 +1,6 @@
 const Tile = () => {
   return (
-    <div className="hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+    <div className="rounded-lg p-3 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
       <p className={`m-0 h-24 max-w-[30ch] text-sm opacity-50 `}>
         Find in-depth information about Next.js features and API.
       </p>
