c6430f57d0ddbfd2f673c42e7e0133d452993e12
RR003 <mail2rahulraja@gmail.com>
2023-12-04 00:14:15 -0500

    commits 12/3

--- src/app/Api/index.js
+++ src/app/Api/index.js
@@ -11,12 +11,23 @@ export const getInitialization = () => api.get(`/initialize/`);
 export const getState = (state) => api.get("/initialize/" + state);
 
 export const getClusters = (stateName, ensembleName, distanceMeasure) =>
-  api.get("/cluster/" + stateName + "/" + ensembleName + "/" + distanceMeasure);
+  api.get(
+    "/cluster/getAll/" + stateName + "/" + ensembleName + "/" + distanceMeasure
+  );
+
+export const getDistrictPlanInformationForSelectedCluster = (clusterId) => {
+  api.get("/dp/getDistrictPlanInfo/" + clusterId);
+};
+
+export const getDistrictPlanGeoJson = (dpId) => {
+  api.get("/dp/getDistrictPlan/" + dpId);
+};
 
 const apis = {
   getInitialization,
   getState,
   getClusters,
+  getDistrictPlanInformationForSelectedCluster,
 };
 
 export default apis;
