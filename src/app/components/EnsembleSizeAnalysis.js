3ca920de9e345672b5080b982ba070b33cccf114
Saketh Kotagiri <saki@Sakeths-MacBook-Pro.local>
2023-12-20 05:11:18 -0500

    chart

--- src/app/components/EnsembleSizeAnalysis.js
+++ src/app/components/EnsembleSizeAnalysis.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import ApexCharts from "react-apexcharts";
+// import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(name, calories, fat, carbs, protein, set1, set2) {
+  return { name, calories, fat, carbs, protein, set1, set2 };
+}
 
 const EnsembleSizeAnalysis = (props) => {
-  // Define your data for ensemble size and corresponding clusters
-
   // Extract ensemble size and clusters data for the chart
   const ensembleSizeData =
     props.state === "Colorado"
@@ -15,7 +25,16 @@ const EnsembleSizeAnalysis = (props) => {
   const numberOfClusters = ensembleSizeData.map(
     (item) => item.numberOfClusters
   );
-
+  var rows = [
+    createData("Num. Plans", 250, 1000, 2000, 3000, 4000, 5000),
+    createData("Num. Clusters", 3, 9, 10, 11, 11, 11),
+  ];
+  if (props.state == "Colorado") {
+    rows = [
+      createData("Num. Plans", 250, 1000, 2000, 3000, 4000, 5000),
+      createData("Num. Clusters", 3, 8, 10, 11, 11, 12),
+    ];
+  }
   const options = {
     series: [
       {
@@ -102,6 +121,37 @@ const EnsembleSizeAnalysis = (props) => {
         width="100%"
         height="350"
       />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          {/* <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead> */}
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.set1}</TableCell>
+                <TableCell align="right">{row.set2}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
